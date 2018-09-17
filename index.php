caa42f2ad00be71926dff78648a14b97458bfcb5
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2018-09-17 14:08:06 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+function countHoles($num) {
+    $numeros = [1,0,0,0,0,0,1,0,2,1];
+    $numArr = str_split($num);
+    $sumHoles = 0;
+    
+    for($i = 0; $i < count($numArr); $i++){
+        $sumHoles += $numeros[$numArr[$i]];
+    }
+    
+    return $sumHoles;
+}
